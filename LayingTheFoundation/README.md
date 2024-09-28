ee2515bd7396f372535a0748d7bf0302831a34c5
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-09-28 13:22:24 +0530

    part1

---
+++ LayingTheFoundation/README.md
@@ -0,0 +1,10 @@
+- To replace the syntex of code like to create "npx parcel index.html" to "npm start" just change from package.json ""scripts": {
+    "start": "parcel index.html",
+    "build": "parcel build index.html",
+    "test": "jest"
+  },"
+
+
+  - to run the command to the server type npx parcel index.html
+
+  - to start type npm run start same as parcel index.html
--- LayingTheFoundation/index.js
+++ LayingTheFoundation/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTMLElement(render)
+// const Heading = () => <h1 id="heading">Namaste React</h1>;
 
-const heading=React.createElement("h1",{id:"heading"},"Namaste react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Heading />);
 
-const root=React.createRoot(document.getElementById("root"));
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"This is Namaste React"),
+        React.createElement("h2",{},"by Akshay Saini")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")
+   
+]),
+]);
 
-root.render(heading);
+console.log(parent);   // object
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
