b9f2174f556b24c096391945e40ae5cdb785b496
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-09-28 13:41:27 +0530

    Part2

--- LayingTheFoundation/index.js
+++ LayingTheFoundation/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const Heading = () => <h1 id="heading">Namaste React</h1>;
+// React.createElement => Object => HTMLElement(render)
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<Heading />);
 
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"This is Namaste React"),
-        React.createElement("h2",{},"by Akshay Saini")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h2 tag")
-   
-]),
-]);
-
-console.log(parent);   // object
+const heading=React.createElement("h1",{id:"heading"},"Namaste React");
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
