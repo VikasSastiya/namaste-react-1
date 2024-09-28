e22748d72b455a4bfdab4a60549b4d787a7b4796
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-09-28 18:35:19 +0530

    part6

--- LayingTheFoundation/index.js
+++ LayingTheFoundation/index.js
@@ -1,28 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-// how to put react element inside element
-
-const elem=<span>React Element</span>
-
-
-
-
 const Title=()=>(
 
    <h1 className="head" tabIndex="5">
-    {elem}
 
     Namaste React using JSX
     </h1>
     ); 
-    // whatever we write in This {} brackets count as javascript code
-    
+      // we can also
+
    const HeadingComponent=()=>(  
      <div id="container">
-        
-            <Title/>             
+        <Title/>
+        {/* {Title()} 
+        <Title/>
+        <Title></Title>     this three things have equal meaning        */}
      <h1 className="heading">Namaste React Functional Component</h1>
      </div>
 );
@@ -32,4 +25,3 @@ const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent/>);
 
-// we cannot use functional component as it is in root.render() we write like this root.render(<HeadingComponent/>); which babel can understand
