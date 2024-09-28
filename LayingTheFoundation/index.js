ff96d627451fd6e489008493f1016da4cb718c7e
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-09-28 14:33:14 +0530

    Part3

--- LayingTheFoundation/index.js
+++ LayingTheFoundation/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTMLElement(render)
+// React.createElement =>gives you an ReactElement-JS Object => HTMLElement(render)
 
 
-const heading=React.createElement("h1",{id:"heading"},"Namaste React");
+const heading=React.createElement("h1",
+    {id:"heading"},
+    "Namaste React"
+       );
 
+console.log(heading);
+
+// JSX (transpiled before it reaches the JS Engine) - transpilation is done by PARCEL - Babel(FOR MORE INFORMATION please must visit babel.io)
+const jsxHeading=(
+   <h1 className="head" tabIndex="1">
+    Namaste React using JSX
+    </h1>
+    );   //  this is JSX-
+// JSX is not html inside javascript ,JSX is a HTML like syntex or XML like syntax
+
+// JSx is syntex ,react element is an object
+
+
+
+console.log(jsxHeading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
