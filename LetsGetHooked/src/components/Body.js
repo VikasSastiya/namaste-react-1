e6e6d50e4e2d84dc5a8720aaa7333482dc6c93a6
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-01 12:43:36 +0530

    Part1

--- LetsGetHooked/src/components/Body.js
+++ LetsGetHooked/src/components/Body.js
@@ -1,1 +1,22 @@
-vddv
+import RestaurantCard from "../components/RestaurantCard"
+import resData from "../utils/mockData";
+
+
+
+const Body=() => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+             {
+                    resData.map((restaurant,index)=>(
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+
+                    )) }
+
+            </div>
+        </div>
+    )
+   }
+
+   export default Body;
