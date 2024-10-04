97ca8f7796e45e9be04cd51d50e9151e6230891f
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-04 15:59:51 +0530

    cors

--- 6.CorsPluginIssueResolve/src/app.js
+++ 6.CorsPluginIssueResolve/src/app.js
@@ -1,1 +1,20 @@
-dfgfd
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+    
+const AppLayout=()=> {
+    console.log(<Body/>);  // this is virtual dom
+    return (
+        <div className="app">
+           <Header/>
+           <Body/>
+        </div>
+    )
+}
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>);
