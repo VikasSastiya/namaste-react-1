a2b2aace021c67d7c168a27bd744a07bb53a0533
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-05 21:13:12 +0530

    part3

---
+++ 7FindingThePath/src/components/Error.js
@@ -0,0 +1,17 @@
+// react-router-dom also gives some hook as useRouterError
+import { useRouteError } from "react-router-dom";  // it will give some more information about error
+
+
+const Error=()=>{
+    const err=useRouteError();
+      console.log(err);
+    return (<div>
+        <h1>OOops!!!</h1>
+        <h2>Something went wrong!!</h2>
+        <h3>{err.status}:{err.statusText}</h3>
+        </div>
+    );
+};
+
+export default Error;
+
