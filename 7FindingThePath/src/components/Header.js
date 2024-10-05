ae3a2d38053e723e461b5491bd2b93a1291bb201
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-05 19:47:02 +0530

    Part2

---
+++ 7FindingThePath/src/components/Header.js
@@ -0,0 +1,54 @@
+import {LOGO_URL} from "../utils/constants";
+import { useState,useEffect } from "react";
+
+
+
+const Header=()=> {
+
+    let btnName="Login";
+
+    const [btnNameReact,setBtnNameReact]=useState("Login");
+    console.log("Header render");
+// as I click to the button react update this btnNameReact variable and also refresh the header component
+    // it trigger the reconcilation cycle
+         useEffect(()=> {
+            console.log("useEffect called");   // if no dependency array => useEffect is called on every render
+         },[btnNameReact]);      //  if dependency array is empty = [] => useEffect is called on initial render(just once)
+         // if dependency array is [btnNameReact] => called everytime btnNameReact is updated
+     
+         // is react refreshing the whole hedderor modifying button
+    // it will rerenderthr whole component- refresh quickly
+
+    // how constant value updated/ modified why?- whenever you update thisvalue it ias calling this function once again when you envok
+    // function btn variable is new variable then it was before
+return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo"
+                 src={LOGO_URL}/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button className="login" 
+                    onClick={ ()=>{
+                        btnNameReact=="Login"?
+                        setBtnNameReact("Logout"):
+                        setBtnNameReact("Login");
+                            // console.log(btnName);
+
+                        // behind the seen as you call setBtnNameReact("Logout");
+                        }
+                        }
+                        >{btnNameReact}</button>
+                        
+                </ul>
+            </div>
+        </div>
+    );
+   };
+
+export default Header;
