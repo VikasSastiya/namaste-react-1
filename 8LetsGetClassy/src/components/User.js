60fa0faf33d24c0e43e55c2255acac3373044dbf
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-11 14:05:22 +0530

    Part2

--- 8LetsGetClassy/src/components/User.js
+++ 8LetsGetClassy/src/components/User.js
@@ -1,5 +1,12 @@
+  import {useState} from "react";
+
 const  User = (props)=> {   // we can write this like "const  User = ({name})=> {"
+       const [count]=useState(0);
+       const [count2]=useState(1);
+
     return <div className="user-card">
+        <h1>Count = {count}</h1>
+        <h1>Count2 = {count2}</h1>
         <h2>Name: {props.name}</h2>
         {/*we can write above code like "<h2>Name: {props.name}</h2>" */}
         <h3>Location: Dehradun</h3>
--- 8LetsGetClassy/src/components/UserClass.js
+++ 8LetsGetClassy/src/components/UserClass.js
@@ -2,18 +2,24 @@ import React from "react";
 // this is how we create class based comonent
 // class Based component is a class which have render function which written JSX 
 class UserClass extends React.Component {   //  react.Component is a class which is there inside react
-
+    // loading a class based component that mean we create an instense in class
     constructor(props){
         super(props);
-
-        console.log(props);
+    this.state={    // state variable is a big object which contain all the object
+         count:0,
+         count2:2,
+    };
+        // console.log(props);
     }
 
     render() {
          const {name,location}=this.props;
+         const {count,count2}=this.state;
 
         return (
             <div className="user-card">
+                <h1>Count : {this.state.count}</h1>
+                <h1>Count2 : {this.state.count2}</h1>
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h4>Contact: @akshaymarch7</h4>
