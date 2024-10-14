4b4de89831c4545be73ab520acd38bc836e2c53c
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-14 14:07:18 +0530

    Part4

--- 9OptimizingOurApp/src/app.js
+++ 9OptimizingOurApp/src/app.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React,{lazy,Suspense} from "react";   
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import Cart from "./components/Cart";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
+
+    // Chunking - to break down our app to smaller chunks so you don't put load on single bundle
+    // Code Splitting
+    // Dynamic bundling
+    // lazy loading - ondemand loading-search it (dynamic import)
+
+         const Grocery=lazy(()=>import("./components/Grocery"));  // lazy is a react package 
     
+       const About=lazy(()=>import("./components/About"));
+
 const AppLayout=()=> {
     console.log(<Body/>);  // this is virtual dom
     return (
@@ -42,11 +52,15 @@ const appRouter=createBrowserRouter([
            },
             {
                 path:"/about",
-                element:<About/>,
+                element:<Suspense fallback={<h1>Loading....</h1>}><About/></Suspense>,
             },
             {
                 path:"/contact",
                 element:<Contact/>,  
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<h1>Loading....</h1>}><Grocery/></Suspense>
             },  
             {
                 path:"/cart",
