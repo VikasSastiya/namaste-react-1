5749798fcb3adfc079aac3405e3e2ec99e038c58
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-14 19:02:52 +0530

    Part1

--- 10JoDikhtaHaiVoBiktaHai/src/utils/constants.js
+++ 10JoDikhtaHaiVoBiktaHai/src/utils/constants.js
@@ -1,1 +1,9 @@
-gf
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO_URL="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg";
+export const Menu_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=";
+
+// note it carefully default can export only one time like we cannot use export default CDN_URL ,export default LOGO_URL
+// two times so weuse the following export at the time of innitialization without using default like as shown above
+
+
+// there are two type of export import ,named export,default export,import
---
+++ 10JoDikhtaHaiVoBiktaHai/src/utils/useRestaurantMenu.js
@@ -0,0 +1,24 @@
+import { useState,useEffect } from "react";
+import { Menu_API } from "../utils/constants";
+
+   // this is how we create useRestaurantMenu() hook
+
+ export const useRestaurantMenu=(resId)=>{
+     // fetchdata
+   
+      const [resInfo,setResInfo]=useState(null);
+
+       useEffect(()=>{
+        fetchData();
+       },[]);
+       
+       const fetchData=async()=>{
+          const data=await fetch(Menu_API+resId);
+
+          const json=await data.json();
+
+           setResInfo(json.data);
+       };
+
+    return resInfo;
+}
