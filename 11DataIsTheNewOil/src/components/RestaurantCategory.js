5a1cf8be572f4d8873c7d01832de531fbdef710b
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-20 15:41:45 +0530

    Part3

---
+++ 11DataIsTheNewOil/src/components/RestaurantCategory.js
@@ -0,0 +1,30 @@
+// import { useState } from "react";
+import ItemList from "./ItemList";
+
+const RestaurantCategory=({data,showItems,setShowIndex})=> {
+
+     const handleClick=()=>{
+        setShowIndex();
+     };
+    // console.log(data);
+    return (
+          <div>
+        {/* Header */}
+        <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between">
+          <div className="flex justify-between cursor-pointer"
+           onClick={handleClick}>
+         <span className="font-bold text-lg ">
+            {data.title} ({data.itemCards.length})
+            </span>
+            </div>
+           
+        {showItems&&<ItemList items={data.itemCards}/>}
+
+        </div>
+       
+        {/* Accordian body */}
+    </div>
+    );
+};
+
+export default RestaurantCategory;
--- 11DataIsTheNewOil/src/components/RestaurantMenu.js
+++ 11DataIsTheNewOil/src/components/RestaurantMenu.js
@@ -1,8 +1,9 @@
-// import {useState,useEffect} from "react";
+import {useState,useEffect} from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 // import { Menu_API } from "../utils/constants";
 import {useRestaurantMenu} from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 
 const RestaurantMenu=()=>{
@@ -14,6 +15,7 @@ const RestaurantMenu=()=>{
 
        const resInfo=useRestaurantMenu(resId);
 
+       const [ShowIndex,setShowIndex]=useState(0);
   //     useEffect(()=>{
   //       fetchMenu();
   //     },[]);   // this is empty dependency array that mean it will call at once at it render
@@ -32,29 +34,43 @@ const RestaurantMenu=()=>{
 
     const {name,costForTwo,cuisines}=resInfo?.cards[2]?.card?.card?.info||[];
     const itemCards=resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card?.itemCards||[];
-    
+    // console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
     // console.log(itemCards);
 
-      
+      const categories=
+      resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(   // this is an accordian just search the code you will get that
+        (c)=>
+          c.card?.card?.["@type"]==                      
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+      );
+      // console.log(categories);
 
     return resInfo==null?(
         <Shimmer/>
     ):(
-        <div className="menu">
+        <div className="text-center ">
       
 {/* this is one way to make the code more proper is hown in upper code */}
-        <h2>{name}</h2>
-        <p>{cuisines} - Rs {costForTwo/100} for two</p>
-        <h2>Menu</h2>
+        <h2 className="font-bold my-6 text-2xl">{name}</h2>
+        <p className="font-bold text-lg">{cuisines} - Rs {costForTwo/100} for two</p>
+        {/* <h2>Menu</h2> */}
        {/* <h2>{description.join(", ")-{defaultPrice}}</h2> */}
        {/* <h3>cost {defaultPrice/100}</h3> */}
-      
-
-       <ul>
+       {/* Categories accordians- just search it you will get the meaning */}
+               {categories.map((category,index)=> (
+                // here parent
+                <RestaurantCategory
+                 key={category?.card?.card.title} 
+                 data={category?.card?.card}
+                 showItems={index==ShowIndex?true:false}
+                 setShowIndex={()=>setShowIndex(index)}
+                 />
+               ))}
+       {/* <ul>
         {itemCards.map((item)=>(
           <li key={item.card.info.id}>{item.card.info.name} - {item.card.info.defaultPrice/100}</li>
         ))}
-       </ul>
+       </ul> */}
         </div>
     );
 };
